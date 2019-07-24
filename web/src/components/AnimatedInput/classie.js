7c9ab43fb7c605c44372d6dc62a0e30298455543
Jesús Gallego Irles <jesusgallego@outlook.com>
2019-07-24 19:36:54 +0200

    Fix document global

--- web/src/components/AnimatedInput/classie.js
+++ web/src/components/AnimatedInput/classie.js
@@ -1,33 +1,11 @@
-function classReg (className) {
-  return new RegExp('(^|\\s+)' + className + '(\\s+|$)')
+function hasClass (elem, c) {
+  return elem.classList.contains(c)
 }
-
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
-var hasClass, addClass, removeClass
-
-if ('classList' in document.documentElement) {
-  hasClass = function (elem, c) {
-    return elem.classList.contains(c)
-  }
-  addClass = function (elem, c) {
-    elem.classList.add(c)
-  }
-  removeClass = function (elem, c) {
-    elem.classList.remove(c)
-  }
-} else {
-  hasClass = function (elem, c) {
-    return classReg(c).test(elem.className)
-  }
-  addClass = function (elem, c) {
-    if (!hasClass(elem, c)) {
-      elem.className = elem.className + ' ' + c
-    }
-  }
-  removeClass = function (elem, c) {
-    elem.className = elem.className.replace(classReg(c), ' ')
-  }
+function addClass (elem, c) {
+  elem.classList.add(c)
+}
+function removeClass (elem, c) {
+  elem.classList.remove(c)
 }
 
 function toggleClass (elem, c) {
