3d6988deb8f6cfc497a64dfaca3f7739a040f9f9
Jesús Gallego Irles <jesusgallego@outlook.com>
2019-08-06 18:01:08 +0200

    Animate Hero illustrations

---
+++ web/src/landing-sections/Hero/animation.js
@@ -0,0 +1,144 @@
+import anime from 'animejs/lib/anime.es.js'
+
+function animateSquares (svg) {
+  const els = svg.querySelectorAll('.square')
+  for (let i = 0; i < els.length; i++) {
+    const el = els[i]
+    var offset = anime.setDashoffset(el)
+    const dasharray = offset / 5
+
+    anime({
+      targets: el,
+      strokeDashoffset: [anime.setDashoffset, 0],
+      strokeDasharray: [dasharray, dasharray],
+      duration: anime.random(1000, 2000),
+      delay: anime.random(0, 1000),
+      loop: true,
+      direction: 'alternate',
+      easing: 'easeInOutSine',
+      autoplay: true
+    })
+  }
+}
+
+function animateDots (svg) {
+  const els = svg.querySelectorAll('.dots circle')
+  for (let i = 0; i < els.length; i++) {
+    const el = els[i]
+    anime({
+      targets: el,
+      duration: anime.random(600, 1000),
+      delay: function (el, i) {
+        return i * 100
+      },
+      translateY: 10,
+      loop: true,
+      direction: 'alternate',
+      easing: 'easeInOutSine',
+      autoplay: true
+    })
+  }
+}
+function animateClouds (svg) {
+  const els = svg.querySelectorAll('.cloud')
+  for (let i = 0; i < els.length; i++) {
+    const el = els[i]
+    anime({
+      targets: el,
+      duration: 1000,
+      delay: anime.stagger(200),
+      translateY: 10,
+      loop: true,
+      direction: 'alternate',
+      easing: 'easeInOutSine',
+      autoplay: true
+    })
+  }
+}
+function animateArrows (svg) {
+  const arrows = svg.querySelectorAll('.arrow')
+  for (let i = 0; i < arrows.length; i++) {
+    const arrow = arrows[i]
+    const body = arrow.querySelector('path')
+    const head = arrow.querySelector('polygon')
+    anime({
+      targets: body,
+      duration: 300,
+      strokeDashoffset: [anime.setDashoffset, 0],
+      delay: 500,
+      easing: 'easeInOutSine',
+      autoplay: true
+    })
+    anime({
+      targets: head,
+      duration: 150,
+      fill: ['none', '#333333'],
+      delay: 800,
+      easing: 'easeInOutSine',
+      autoplay: true
+    })
+  }
+}
+function animateCode (svg) {
+  const els = svg.querySelectorAll('.code rect')
+  for (let i = 0; i < els.length; i++) {
+    const el = els[i]
+    const width = anime.get(el, 'width')
+    anime({
+      targets: el,
+      duration: anime.random(1000, 2000),
+      delay: (el, i) => i * 100,
+      width: [10, width],
+      loop: true,
+      direction: 'alternate',
+      easing: 'easeInOutSine',
+      autoplay: true
+    })
+  }
+}
+function animateLine (svg) {
+  const els = svg.querySelectorAll('.line')
+  for (let i = 0; i < els.length; i++) {
+    const el = els[i]
+    const off = anime.setDashoffset(el)
+    anime({
+      targets: el,
+      strokeDashoffset: [off, 0],
+      strokeDasharray: [off / 4, off / 4],
+      duration: 1500,
+      loop: true,
+      easing: 'easeInOutQuad',
+      autoplay: true
+    })
+  }
+}
+function animateCircles (svg) {
+  const els = svg.querySelectorAll('.circle')
+  for (let i = 0; i < els.length; i++) {
+    const el = els[i]
+    return anime({
+      targets: el,
+      r: 10,
+      easing: 'easeInOutSine',
+      duration: anime.random(600, 1000),
+      delay: anime.random(0, 200),
+      loop: true,
+      direction: 'alternate',
+      autoplay: true
+    })
+  }
+}
+
+function init (obj) {
+  // obj.addEventListener('load', () => {
+  animateSquares(obj.contentDocument)
+  animateDots(obj.contentDocument)
+  animateClouds(obj.contentDocument)
+  animateCode(obj.contentDocument)
+  animateArrows(obj.contentDocument)
+  animateLine(obj.contentDocument)
+  animateCircles(obj.contentDocument)
+  // })
+}
+
+export default init
--- web/src/landing-sections/Hero/index.js
+++ web/src/landing-sections/Hero/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Slider from 'react-slick'
 
 import styles from './hero.module.css'
+import animate from './animation'
 
 const Hero = ({ carousel }) => {
+  const initAnimations = useRef([])
   return (
     <section id='start' className={styles.hero}>
       <h1>
@@ -18,9 +20,30 @@ const Hero = ({ carousel }) => {
       <a href='#contact' className={styles.contact}>
         CONTACTO
       </a>
-      <Slider className={styles.carousel} autoplay autoplaySpeed={4000} dots={false} arrows={false}>
-        {carousel.map(image => (
-          <object data={image} alt='decorative landing' className={styles.carousel__image} />
+      <Slider
+        className={styles.carousel}
+        autoplay
+        autoplaySpeed={4000}
+        dots={false}
+        arrows={false}
+        onReInit={() => {
+          const objects = document.querySelectorAll('object')
+          for (let i = 0; i < objects.length; i++) {
+            const obj = objects[i]
+            if (!initAnimations.current.includes(obj)) {
+              initAnimations.current.push(obj)
+              animate(obj)
+            }
+          }
+        }}
+      >
+        {carousel.map((image, i) => (
+          <object
+            key={i}
+            data={image}
+            alt='decorative landing'
+            className={styles.carousel__image}
+          />
         ))}
       </Slider>
     </section>
