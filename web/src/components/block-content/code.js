037269ee00b4037ca971ecadbb928888d0a26c22
Jesús Gallego Irles <jesusgallego@outlook.com>
2019-11-04 13:25:36 +0100

    Code syntax highlighter style

--- web/src/components/block-content/code.js
+++ web/src/components/block-content/code.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
+import darcula from 'react-syntax-highlighter/dist/cjs/styles/hljs/darcula'
 
 function Code ({ language, code }) {
   if (!code) return null
   return (
-    <SyntaxHighlighter language={language || 'text'} showLineNumbers>
+    <SyntaxHighlighter language={language || 'text'} showLineNumbers wrapLines style={darcula}>
       {code}
     </SyntaxHighlighter>
   )
