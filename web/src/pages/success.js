7f3a7ef5fa42002b9310d17dc9d3f4e39d291ff6
Jesús Gallego Irles <jesusgallego@outlook.com>
2020-06-26 10:39:45 +0200

    FB Pixel Compra

--- web/src/pages/success.js
+++ web/src/pages/success.js
@@ -7,6 +7,19 @@ const Success = ({ location }) => {
   const params = parseQueryStrings(location.search)
   return (
     <Layout>
+      <script>
+        {`
+          fbq('track', 'Purchase', {
+            value: 15,00,
+            currency: 'EUR',
+            contents: [
+                {
+                    id: 'Comprar'
+                }
+            ],
+          });
+          `}
+      </script>
       <Container>
         <h1>Enhorabuena</h1>
         {params && params.session_id && <div>{params.session_id}</div>}
