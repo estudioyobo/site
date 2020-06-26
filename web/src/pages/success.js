10ce380e63f9e108ae44771c6810520360a8e49f
Jesús Gallego Irles <jesusgallego@outlook.com>
2020-06-26 11:49:22 +0200

    fbq in global

--- web/src/pages/success.js
+++ web/src/pages/success.js
@@ -1,33 +1,28 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
+import React, { useEffect } from 'react'
 import Layout from '../components/layout'
 import Container from '../components/container'
 import parseQueryStrings from '../lib/querystrings'
 
 const Success = ({ location }) => {
   const params = parseQueryStrings(location.search)
+  useEffect(() => {
+    global.fbq('track', 'Purchase', {
+      value: 15.0,
+      currency: 'EUR',
+      contents: [
+        {
+          id: 'Comprar'
+        }
+      ]
+    })
+  }, [])
   return (
-    <>
-      <script>
-        {`
-          fbq(\"track\", \"Purchase\", {
-            value: 15.00,
-            currency: \"EUR\",
-            contents: [
-                {
-                    id: \"Comprar\"
-                }
-            ],
-          });
-          `}
-      </script>
-      <Layout>
-        <Container>
-          <h1>Enhorabuena</h1>
-          {params && params.session_id && <div>{params.session_id}</div>}
-        </Container>
-      </Layout>
-    </>
+    <Layout>
+      <Container>
+        <h1>Enhorabuena</h1>
+        {params && params.session_id && <div>{params.session_id}</div>}
+      </Container>
+    </Layout>
   )
 }
 
