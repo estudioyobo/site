7df3711c05a57ade0ccd75f14df6e27856f814b9
Jesús Gallego Irles <jesusgallego@outlook.com>
2020-07-02 17:47:12 +0200

    © 2020

--- web/src/components/Footer/index.js
+++ web/src/components/Footer/index.js
@@ -6,60 +6,65 @@ import Newsletter, { Button } from '../newsletter'
 import AnimatedLink from '../AnimatedLink'
 import Social from '../Social'
 
-const modalStyles= {
+const modalStyles = {
   overlay: {
-backgroundColor: "transparent",
-zIndex: 200
+    backgroundColor: 'transparent',
+    zIndex: 200
   },
   content: {
-    left: "40%",
-    top: "auto"
+    left: '40%',
+    top: 'auto'
   }
-
 }
 
 const Footer = ({ companyInfo }) => {
   const [newsletter, setNewsletter] = useState(false)
   return (
     <footer>
-      <div className='row margin equal elements'>
-        <ul className='footer-links'>
+      <div className="row margin equal elements">
+        <ul className="footer-links">
           <li>
-            <AnimatedLink href='/privacy'>Política de privacidad</AnimatedLink>
+            <AnimatedLink href="/privacy">Política de privacidad</AnimatedLink>
           </li>
           <li>
-            <AnimatedLink href='/legal'>Aviso Legal</AnimatedLink>
+            <AnimatedLink href="/legal">Aviso Legal</AnimatedLink>
           </li>
           <li>
-            <AnimatedLink href='/404'>Error 404</AnimatedLink>
+            <AnimatedLink href="/404">Error 404</AnimatedLink>
           </li>
         </ul>
-        <div className='footer-contact'>
-          <div className='footer-contact--social'>
+        <div className="footer-contact">
+          <div className="footer-contact--social">
             {companyInfo.social.map((e, i) => (
               <Social key={i} {...e} />
             ))}
           </div>
-          <div className='footer-contact--mail'>
+          <div className="footer-contact--mail">
             <AnimatedLink href={`mailto:${companyInfo.email}`}>{companyInfo.email}</AnimatedLink>
           </div>
-          <div className='footer-phone'>
+          <div className="footer-phone">
             <AnimatedLink href={`tel:${companyInfo.phone}`}>{companyInfo.phone}</AnimatedLink>
           </div>
         </div>
-        <div className='footer-logo'>
-          <Logo color='#F3F4F0' width='200px' />
-          <Button small onClick={() => setNewsletter(true)}>Suscríbete a la newsletter!</Button>
+        <div className="footer-logo">
+          <Logo color="#F3F4F0" width="200px" />
+          <Button small onClick={() => setNewsletter(true)}>
+            Suscríbete a la newsletter!
+          </Button>
         </div>
       </div>
-      <div className='row'>
-        <div className='made'>Made with &#x2764; by Estudio Yobo</div>
+      <div className="row">
+        <div className="made">Made with &#x2764; by Estudio Yobo</div>
       </div>
-      <div className='row'>
-        <div className='copyright'>Copyright &copy; 2019. Estudio Yobo</div>
+      <div className="row">
+        <div className="copyright">Copyright &copy; 2020. Estudio Yobo</div>
       </div>
       <Modal isOpen={newsletter} onRequestClose={() => setNewsletter(false)} style={modalStyles}>
-        <Newsletter title='Suscríbete a la newsletter' subtitle='y entérate de noticias del mundo del diseño y desarrollo del mundo digital' path='footer' />
+        <Newsletter
+          title="Suscríbete a la newsletter"
+          subtitle="y entérate de noticias del mundo del diseño y desarrollo del mundo digital"
+          path="footer"
+        />
       </Modal>
     </footer>
   )
